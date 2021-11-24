8f3f3f82c69ea2a14a97f96178203ce0e31db679
Dennizz <root@ipaddr.nl>
2021-11-24 16:51:27 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,72 @@
+# Aviatrix Spoke VPC
+resource "aviatrix_vpc" "default" {
+  cloud_type           = local.cloud_type
+  region               = local.cloud == "gcp" ? null : var.region
+  cidr                 = local.cloud == "gcp" ? null : var.cidr
+  account_name         = var.account
+  name                 = local.name
+  aviatrix_transit_vpc = true
+  aviatrix_firenet_vpc = false
+  resource_group       = var.resource_group
+
+  dynamic "subnets" {
+    for_each = local.cloud == "gcp" ? ["dummy"] : [] #Trick to make block conditional. Count not available on dynamic blocks.
+    content {
+      name   = local.name
+      cidr   = var.cidr
+      region = var.region
+    }
+  }
+
+  dynamic "subnets" {
+    for_each = length(var.ha_region) > 0 ? ["dummy"] : [] #Trick to make block conditional. Count not available on dynamic blocks.
+    content {
+      name   = "${local.name}-ha"
+      cidr   = var.ha_cidr
+      region = var.ha_region
+    }
+  }
+}
+
+#Spoke GW
+resource "aviatrix_transit_gateway" "default" {
+  enable_active_mesh               = var.active_mesh
+  cloud_type                       = local.cloud_type
+  vpc_reg                          = local.region
+  gw_name                          = local.name
+  gw_size                          = local.instance_size
+  vpc_id                           = var.use_existing_vpc ? var.vpc_id : (local.cloud == "oci" ? aviatrix_vpc.default[0].name : aviatrix_vpc.default[0].vpc_id)
+  account_name                     = var.account
+  subnet                           = local.subnet
+  ha_subnet                        = var.ha_gw ? local.ha_subnet : null
+  ha_gw_size                       = var.ha_gw ? local.instance_size : null
+  ha_zone                          = var.ha_gw ? local.ha_zone : null
+  connected_transit                = var.connected_transit
+  enable_hybrid_connection         = var.hybrid_connection
+  bgp_manual_spoke_advertise_cidrs = var.bgp_manual_spoke_advertise_cidrs
+  enable_learned_cidrs_approval    = var.learned_cidr_approval
+  learned_cidrs_approval_mode      = var.learned_cidrs_approval_mode
+  enable_segmentation              = var.enable_segmentation
+  insane_mode                      = var.insane_mode
+  insane_mode_az                   = local.insane_mode_az
+  ha_insane_mode_az                = var.ha_gw ? local.ha_insane_mode_az : null
+  single_az_ha                     = var.single_az_ha
+  single_ip_snat                   = var.single_ip_snat
+  enable_advertise_transit_cidr    = var.enable_advertise_transit_cidr
+  enable_firenet                   = var.enable_firenet
+  enable_transit_firenet           = var.enable_transit_firenet
+  enable_egress_transit_firenet    = var.enable_egress_transit_firenet
+  bgp_polling_time                 = var.bgp_polling_time
+  bgp_ecmp                         = var.bgp_ecmp
+  local_as_number                  = var.local_as_number
+  enable_bgp_over_lan              = var.enable_bgp_over_lan
+  enable_encrypt_volume            = var.enable_encrypt_volume
+  customer_managed_keys            = var.customer_managed_keys
+  tunnel_detection_time            = var.tunnel_detection_time
+  tags                             = var.tags
+  availability_domain              = local.cloud == "oci" ? aviatrix_vpc.default[0].availability_domains[0] : null
+  fault_domain                     = local.cloud == "oci" ? aviatrix_vpc.default[0].fault_domains[0] : null
+  ha_availability_domain           = var.ha_gw ? (local.cloud == "oci" ? aviatrix_vpc.default[0].availability_domains[1] : null) : null
+  ha_fault_domain                  = var.ha_gw ? (local.cloud == "oci" ? aviatrix_vpc.default[0].fault_domains[1] : null) : null
+  enable_multi_tier_transit        = var.enable_multi_tier_transit
+}
---
+++ variables.tf
@@ -0,0 +1,321 @@
+variable "cloud" {
+  description = "Cloud type"
+  type        = string
+
+  validation {
+    condition     = contains(["aws", "azure", "oci", "ali", "gcp"], lower(var.cloud))
+    error_message = "Invalid cloud type. Choose AWS, Azure, GCP, ALI or OCI."
+  }
+}
+
+variable "name" {
+  description = "Name for this transit VPC and it's gateways"
+  type        = string
+}
+
+variable "prefix" {
+  description = "Boolean to determine if name will be prepended with avx-"
+  type        = bool
+  default     = true
+}
+
+variable "suffix" {
+  description = "Boolean to determine if name will be appended with -transit"
+  type        = bool
+  default     = true
+}
+
+variable "region" {
+  description = "The region to deploy this module in"
+  type        = string
+}
+
+variable "connected_transit" {
+  description = "Set to false to disable connected transit."
+  type        = bool
+  default     = true
+}
+
+variable "hybrid_connection" {
+  description = "Set to true to prepare Aviatrix transit for TGW connection."
+  type        = bool
+  default     = false
+}
+
+variable "bgp_manual_spoke_advertise_cidrs" {
+  description = "Define a list of CIDRs that should be advertised via BGP."
+  type        = string
+  default     = ""
+}
+
+variable "learned_cidr_approval" {
+  description = "Set to true to enable learned CIDR approval."
+  type        = string
+  default     = "false"
+}
+
+variable "learned_cidrs_approval_mode" {
+  description = "Learned cidrs approval mode. Defaults to Gateway. Valid values: gateway, connection"
+  type        = string
+  default     = null
+}
+
+variable "enable_segmentation" {
+  description = "Switch to true to enable transit segmentation"
+  type        = bool
+  default     = false
+}
+
+variable "ha_region" {
+  description = "Secondary GCP region where subnet and HA Aviatrix Transit Gateway will be created"
+  type        = string
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR range to be used for the VPC"
+  type        = string
+  default     = ""
+}
+
+variable "ha_cidr" {
+  description = "CIDR of the HA GCP subnet"
+  type        = string
+  default     = ""
+}
+
+variable "enable_firenet" {
+  description = "Sign of readiness for FireNet connection"
+  type        = bool
+  default     = false
+}
+
+variable "enable_transit_firenet" {
+  description = "Sign of readiness for Transit FireNet connection"
+  type        = bool
+  default     = false
+}
+
+variable "enable_egress_transit_firenet" {
+  description = "Enable Egress Transit FireNet"
+  type        = bool
+  default     = false
+}
+
+variable "bgp_polling_time" {
+  description = "BGP route polling time. Unit is in seconds"
+  type        = string
+  default     = "50"
+}
+
+variable "bgp_ecmp" {
+  description = "Enable Equal Cost Multi Path (ECMP) routing for the next hop"
+  type        = bool
+  default     = false
+}
+
+variable "enable_multi_tier_transit" {
+  description = "Set to true to enable multi tier transit."
+  type        = bool
+  default     = false
+}
+
+variable "local_as_number" {
+  description = "Changes the Aviatrix Transit Gateway ASN number before you setup Aviatrix Transit Gateway connection configurations."
+  type        = string
+  default     = null
+}
+
+variable "enable_bgp_over_lan" {
+  description = "Enable BGp over LAN. Creates eth4 for integration with SDWAN for example"
+  type        = bool
+  default     = false
+}
+
+variable "account" {
+  description = "The AWS account name, as known by the Aviatrix controller"
+  type        = string
+}
+
+variable "instance_size" {
+  description = "Instance size for the Aviatrix gateways"
+  type        = string
+  default     = ""
+}
+
+variable "ha_gw" {
+  description = "Boolean to determine if module will be deployed in HA or single mode"
+  type        = bool
+  default     = true
+}
+
+variable "insane_mode" {
+  description = "Set to true to enable Aviatrix high performance encryption."
+  type        = bool
+  default     = false
+}
+
+variable "az1" {
+  description = "Concatenates with region to form az names. e.g. eu-central-1a. Only used for insane mode"
+  type        = string
+  default     = ""
+}
+
+variable "az2" {
+  description = "Concatenates with region to form az names. e.g. eu-central-1b. Only used for insane mode"
+  type        = string
+  default     = ""
+}
+
+variable "az_support" {
+  description = "Set to true if the Azure region supports AZ's"
+  type        = bool
+  default     = true
+}
+
+variable "active_mesh" {
+  description = "Set to false to disable active mesh."
+  type        = bool
+  default     = true
+}
+
+variable "single_az_ha" {
+  description = "Set to true if Controller managed Gateway HA is desired"
+  type        = bool
+  default     = true
+}
+
+variable "single_ip_snat" {
+  description = "Specify whether to enable Source NAT feature in single_ip mode on the gateway or not. Please disable AWS NAT instance before enabling this feature. Currently only supports AWS(1) and AZURE(8). Valid values: true, false."
+  type        = bool
+  default     = false
+}
+
+variable "enable_encrypt_volume" {
+  description = "Enable EBS volume encryption for Gateway. Only supports AWS and AWSGOV provider. Valid values: true, false. Default value: false"
+  type        = bool
+  default     = false
+}
+
+variable "customer_managed_keys" {
+  description = "Customer managed key ID for EBS Volume encryption."
+  type        = string
+  default     = null
+}
+
+variable "tunnel_detection_time" {
+  description = "The IPsec tunnel down detection time for the Transit Gateway in seconds. Must be a number in the range [20-600]."
+  type        = number
+  default     = null
+}
+
+variable "tags" {
+  description = "Map of tags to assign to the gateway."
+  type        = map(string)
+  default     = null
+}
+
+variable "china" {
+  description = "Set to true if deploying this module in AWS/Azure China."
+  type        = bool
+  default     = false
+}
+
+variable "gov" {
+  description = "Set to true if deploying this module in AWS/Azure GOV."
+  type        = bool
+  default     = false
+}
+
+variable "resource_group" {
+  description = "Provide the name of an existing resource group."
+  type        = string
+  default     = null
+}
+
+locals {
+  cloud                 = lower(var.cloud)
+  prefix                = var.prefix ? "avx-" : ""
+  suffix                = var.suffix ? "-transit" : ""
+  lower_name            = length(var.name) > 0 ? replace(lower(var.name), " ", "-") : replace(lower(var.region), " ", "-")
+  name                  = "${local.prefix}${local.lower_name}${local.suffix}"
+  cidr                  = var.cidr
+  cidrbits              = tonumber(split("/", local.cidr)[1])
+  newbits               = 26 - local.cidrbits
+  netnum                = pow(2, local.newbits)
+  insane_mode_subnet    = cidrsubnet(local.cidr, local.newbits, local.netnum - 2)
+  ha_insane_mode_subnet = cidrsubnet(local.cidr, local.newbits, local.netnum - 1)
+
+  az1 = length(var.az1) > 0 ? var.az1 : lookup(local.az1_map, local.cloud, null)
+  az1_map = {
+    azure = var.az_support ? "az-1" : null,
+    aws   = "a",
+    gcp   = "b",
+  }
+
+  az2 = length(var.az2) > 0 ? var.az2 : lookup(local.az2_map, local.cloud, null)
+  az2_map = {
+    azure = var.az_support ? "az-2" : null,
+    aws   = "b",
+    gcp   = "c",
+  }
+
+  subnet = var.use_existing_vpc ? var.gw_subnet : (var.insane_mode ? local.insane_mode_subnet : (local.cloud == "gcp" ? aviatrix_vpc.default[0].subnets[local.subnet_map[local.cloud]].cidr : aviatrix_vpc.default[0].public_subnets[local.subnet_map[local.cloud]].cidr))
+  subnet_map = {
+    azure = 0,
+    aws   = var.insane_mode ? cidrsubnet(var.cidr, local.newbits, local.netnum - 2) : aviatrix_vpc.default.public_subnets[0].cidr,
+    gcp   = 0,
+    oci   = 0,
+    ali   = 0,
+  }
+
+  ha_subnet = var.use_existing_vpc ? (contains(["azure", "oci"], local.cloud) ? var.gw_subnet : var.hagw_subnet) : (var.insane_mode ? local.ha_insane_mode_subnet : (local.cloud == "gcp" ? aviatrix_vpc.default[0].subnets[local.ha_subnet_map[local.cloud]].cidr : aviatrix_vpc.default[0].public_subnets[local.ha_subnet_map[local.cloud]].cidr))
+  ha_subnet_map = {
+    azure = 0,
+    aws   = var.insane_mode ? cidrsubnet(var.cidr, local.newbits, local.netnum - 1) : aviatrix_vpc.default.public_subnets[2].cidr,
+    gcp   = length(var.ha_region) > 0 ? 1 : 0
+    oci   = 0,
+    ali   = 1,
+  }
+
+  region  = local.cloud == "gcp" ? "${var.region}-${local.az1}" : var.region
+  ha_zone = local.cloud == "gcp" ? (length(var.ha_region) > 0 ? "${var.ha_region}-${local.az2}" : "${var.region}-${local.az2}") : null
+
+  insane_mode_az = var.insane_mode ? lookup(local.insane_mode_az_map, local.cloud, null) : null
+  insane_mode_az_map = {
+    aws = "${var.region}${var.az1}",
+  }
+
+  ha_insane_mode_az = var.insane_mode ? lookup(local.ha_insane_mode_az_map, local.cloud, null) : null
+  ha_insane_mode_az_map = {
+    aws = "${var.region}${var.az2}",
+  }
+
+  cloud_type = var.china ? lookup(local.cloud_type_map_china, local.cloud, null) : (var.gov ? lookup(local.cloud_type_map_gov, local.cloud, null) : lookup(local.cloud_type_map, local.cloud, null))
+  cloud_type_map = {
+    azure = 8,
+    aws   = 1,
+    gcp   = 4,
+    oci   = 16,
+    ali   = 8192,
+  }
+
+  cloud_type_map_china = {
+    azure = 2048,
+    aws   = 1024,
+  }
+
+  cloud_type_map_gov = {
+    azure = 32,
+    aws   = 256,
+  }
+
+  instance_size = length(var.instance_size) > 0 ? var.instance_size : lookup(local.instance_size_map, local.cloud, null)
+  instance_size_map = {
+    azure = "Standard_B1ms",
+    aws   = "t3.medium",
+    gcp   = "n1-standard-1",
+    oci   = "VM.Standard2.2",
+    ali   = "ecs.g5ne.large",
+  }
+}
