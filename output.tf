7dc0acaa446e5257508a00eb2364d799e28c8a89
Dennizz <root@ipaddr.nl>
2021-11-25 15:52:24 +0100

    fix output

--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@ output "vpc" {
 
 output "transit_gateway" {
   description = "The created Aviatrix spoke gateway as an object with all of it's attributes."
-  value       = aviatrix_spoke_gateway.default
+  value       = aviatrix_transit_gateway.default
 }
