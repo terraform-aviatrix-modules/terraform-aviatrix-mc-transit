2cc3dda7c25da4cfc33da889409c18e08c08e962
Dennizz <root@ipaddr.nl>
2022-01-19 12:23:09 +0100

    add provider version constraint

--- versions.tf
+++ versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     aviatrix = {
-      source = "aviatrixsystems/aviatrix"
+      source  = "aviatrixsystems/aviatrix"
+      version = ">=2.19.0"
     }
   }
   required_version = ">= 0.13"
