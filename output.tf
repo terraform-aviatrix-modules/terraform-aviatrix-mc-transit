1970a961b1d091e15f67e52c0058645e7d1f6a18
Dennizz <root@ipaddr.nl>
2022-03-10 13:54:04 +0100

    add additional outputs to be consumed by mc-firenet module.

--- output.tf
+++ output.tf
@@ -7,3 +7,14 @@ output "transit_gateway" {
   description = "The created Aviatrix Transit Gateway as an object with all of it's attributes."
   value       = aviatrix_transit_gateway.default
 }
+
+#Firenet details is used when this module is combined with the mc-firenet module to propagate details between modules.
+output "mc_firenet_details" {
+  value = {
+    name         = local.name,
+    az1          = local.az1,
+    az2          = local.az2,
+    single_az_ha = var.single_az_ha,
+    ha_gw        = var.ha_gw,
+  }
+}
