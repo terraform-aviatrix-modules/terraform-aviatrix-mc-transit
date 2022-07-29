a2cb7e57217c2199cd89568a38495155bd1e2bb4
Dennizz <root@ipaddr.nl>
2022-07-29 15:24:53 +0200

    fix versions

--- versions.tf
+++ versions.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
-    # aviatrix = {
-    #   source  = "aviatrixsystems/aviatrix"
-    #   version = "~>2.23.0"
-    # }
     aviatrix = {
-      source  = "aviatrix.com/aviatrix/aviatrix"
-      version = "99.0.0"
+      source  = "aviatrixsystems/aviatrix"
+      version = "~>2.23.0"
     }
   }
   required_version = ">= 1.1.1"
