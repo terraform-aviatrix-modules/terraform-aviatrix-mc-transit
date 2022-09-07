bd348348ddf756a3f17c2157cdcb28e686fe2ba3
conip <51626146+conip@users.noreply.github.com>
2022-09-07 10:39:18 +0200

    Create main.tf

---
+++ examples/azure_preserving_eip/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "example" {
+  name     = "PIP-RG"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "pip1" {
+  name                = "PIP-TrGW"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+resource "azurerm_public_ip" "pip2" {
+  name                = "PIP-TrGW-ha"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+module "mc-transit" {
+  source                           = "terraform-aviatrix-modules/mc-transit/aviatrix"
+  version                          = "2.2.1"
+  resource_group                   = azurerm_resource_group.example.name
+  cloud                            = "Azure"
+  region                           = var.azure_region
+  cidr                             = "10.17.0.0/23"
+  account                          = var.avx_ctrl_account_azure
+ 
+  allocate_new_eip                 = false
+  eip                              = azurerm_public_ip.pip1.ip_address
+  azure_eip_name_resource_group    = "PIP-TrGW:${azurerm_resource_group.example.name}"
+  ha_eip                           = azurerm_public_ip.pip2.ip_address
+  ha_azure_eip_name_resource_group = "PIP-TrGW-ha:${azurerm_resource_group.example.name}"
+
+  depends_on = [
+    azurerm_public_ip.pip1,
+    azurerm_public_ip.pip2
+  ]
+}
